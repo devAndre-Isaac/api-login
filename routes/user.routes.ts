4d9e73f10e2cb18fb6b08f7f9ab132eec1d20d13
devAndre-Isaac <andreisaac344@gmail.com>
2021-07-24 19:50:02 -0300

    refactor: fix message to response

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const userRouter = Router();
 
 userRouter.get("/", (request, response) => {
-  return response.json({ message: "Funcionando PORRAAAA" });
+  return response.json({ message: "Ok" });
 });
 
 export default userRouter;
