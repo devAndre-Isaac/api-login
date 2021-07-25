1fa043193362f9cfc36a34dce86c762817ab23ef
devAndre-Isaac <andreisaac344@gmail.com>
2021-07-25 19:12:06 -0300

    feat: add a Post router

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
 
 const userRouter = Router();
 
 userRouter.get("/", (request, response) => {
   return response.json({ message: "Ok" });
+});
+
+userRouter.post("/userCreate", (request, response) => {
+  return response.status(201).json({ message: "OK" });
 });
 
 export default userRouter;
