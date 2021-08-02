fb4c6fc0151f310225ee71fc5da29877aca206bd
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-02 16:13:24 -0300

    feat: add a interface for update response

--- src/interfaces/user.ts
+++ src/interfaces/user.ts
@@ -8,3 +8,18 @@ export interface UserInterface {
   updated_at?: Date;
   created_at?: Date;
 }
+export interface UpdateUserResponse {
+  name: string;
+  email: string;
+  password: string;
+
+  company: string;
+  id: string;
+  updated_at: Date;
+  created_at: Date;
+}
+export interface UserUpdateInterface {
+  name: string;
+  email: string;
+  password: string;
+}
--- src/repositories/user.ts
+++ src/repositories/user.ts
@@ -1,10 +1,26 @@
-import { UserInterface } from "../interfaces/user";
+import {
+  UpdateUserResponse,
+  UserInterface,
+  UserUpdateInterface,
+} from "../interfaces/user";
 import { User } from "../entity/user";
-import { getRepository } from "typeorm";
+import { createQueryBuilder, getRepository } from "typeorm";
 
 export const create = async (
   UserObject: UserInterface
 ): Promise<UserInterface> => {
   const userEntity = Object.assign(new User(), UserObject);
   return getRepository(User).save(userEntity);
+};
+
+export const updateById = async (
+  updateBody: UserUpdateInterface,
+  id: string
+): Promise<UpdateUserResponse[]> => {
+  const updateRes = await createQueryBuilder()
+    .update(User, Object.assign(new User(), { ...updateBody, id }))
+    .where({ id })
+    .returning("*")
+    .execute();
+  return updateRes.raw;
 };
