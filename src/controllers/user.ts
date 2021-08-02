554b99b99d4cdd18a3eabda98a1e77be28ae6ec0
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-02 16:12:47 -0300

    feat: add a controller for put Method

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,17 @@
-import { UserInterface } from "../interfaces/user";
+import { UserInterface, UserUpdateInterface } from "../interfaces/user";
 import * as repository from "../repositories/user";
 
 export const create = async (
   userBody: UserInterface
 ): Promise<UserInterface> => {
   return repository.create(userBody);
+};
+
+export const updateUserById = async (
+  id: string,
+  userBody: UserUpdateInterface
+): Promise<UserInterface> => {
+  const [user] = await repository.updateById(userBody, id);
+
+  return user;
 };
