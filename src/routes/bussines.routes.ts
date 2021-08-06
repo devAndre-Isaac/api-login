ee8295b7dbaf5e5fd70954f54854543310362fec
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-06 12:59:57 -0300

    feat: add route for bussines

---
+++ src/routes/bussines.routes.ts
@@ -0,0 +1,53 @@
+import { Request, Response, Router } from "express";
+import { param, validationResult } from "express-validator";
+
+import * as validation from "../validation/bussinesValidator";
+import * as controllers from "../controllers/bussines";
+import { getValidData } from "../validation/validatorHandle";
+
+const bussinesRouter = Router();
+
+bussinesRouter.get("/", async (request, response) => {
+  const bussinesGet = await controllers.getAllBussines();
+  return response.status(200).json(bussinesGet);
+});
+
+bussinesRouter.post(
+  "/createBussines",
+  validation.createBussinesValidator,
+  async (request: Request, response: Response) => {
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+    const bussines = await controllers.create(request.body);
+    return response.status(201).json(bussines);
+  }
+);
+
+bussinesRouter.put(
+  "/:id",
+  validation.updateBussinesValidator,
+  async (request: Request, response: Response) => {
+    const { params } = getValidData(request);
+    const bussinesUpdate = await controllers.updateBussinesById(
+      params.id,
+      request.body
+    );
+
+    return response.status(200).json(bussinesUpdate);
+  }
+);
+
+bussinesRouter.delete(
+  "/:id",
+  validation.idValidator,
+  async (request: Request, response: Response) => {
+    const { params } = getValidData(request);
+    const bussinesDelete = await controllers.remove(params.id);
+
+    return response.status(204).json(bussinesDelete);
+  }
+);
+
+export default bussinesRouter;
