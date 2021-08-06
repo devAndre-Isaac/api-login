f617d9fa5ab1c192b7cacecef1cc25b12b5749a4
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-06 13:01:29 -0300

    feat: insert bussinesRouter in index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import bussinesRouter from "./bussines.routes";
 import userRouter from "./user.routes";
 
 const routes = Router();
 
 routes.use("/users", userRouter);
+routes.use("/bussines", bussinesRouter);
 
 export default routes;
