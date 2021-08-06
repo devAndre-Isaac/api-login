05eeabcc4a1b10f23a892d34270d6d2c6edbf922
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-06 13:01:57 -0300

    feat: add controller for bussines

---
+++ src/controllers/bussines.ts
@@ -0,0 +1,31 @@
+import {
+  BussinesUpdateInterface,
+  BussinesInterface,
+} from "../interfaces/bussines";
+import * as repository from "../repositories/bussines";
+import { HttpError } from "../validation/HttpError";
+
+export const create = async (
+  bussinesBody: BussinesInterface
+): Promise<BussinesInterface> => {
+  return repository.create(bussinesBody);
+};
+
+export const updateBussinesById = async (
+  id: string,
+  bussinesBody: BussinesUpdateInterface
+): Promise<BussinesInterface> => {
+  const [bussines] = await repository.updateById(bussinesBody, id);
+
+  return bussines;
+};
+
+export const getAllBussines = async (): Promise<BussinesInterface[]> => {
+  return repository.getAll();
+};
+
+export const remove = async (id: string): Promise<void> => {
+  const result = await repository.remove(id);
+
+  if (!result.affected) throw new HttpError(404, "Bussines not found");
+};
