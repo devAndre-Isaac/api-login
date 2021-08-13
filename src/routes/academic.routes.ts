4e39987cdb8fe92e57884be1fef89d99c6e84695
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-13 11:56:12 -0300

    feat: :sparkles: add post method for academic route
    add a create method and connect with db

--- src/routes/academic.routes.ts
+++ src/routes/academic.routes.ts
@@ -1,10 +1,20 @@
 import { request, Request, response, Response, Router } from "express";
 import { validationResult } from "express-validator";
+import { getRepository } from "typeorm";
+import { Academic } from "../entity/academic";
 
 const academicRouter = Router();
 
 academicRouter.get("/", async (request, response) => {
   return response.json({ message: "Exquece" });
+});
+
+academicRouter.post("/", async (request, response) => {
+  const repository = getRepository(Academic);
+  const academicToSave = repository.create(request.body);
+  const academic = await repository.save(academicToSave);
+
+  return response.status(201).json(academic);
 });
 
 export default academicRouter;
