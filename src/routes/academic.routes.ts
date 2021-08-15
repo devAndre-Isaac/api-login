2fc1bde15c800522e703fb3b79aad61a62e73ada
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-14 23:03:42 -0300

    feat: :recycle: change route for connect with controller
    change route for connect with controller of academic

--- src/routes/academic.routes.ts
+++ src/routes/academic.routes.ts
@@ -2,6 +2,8 @@ import { request, Request, response, Response, Router } from "express";
 import { validationResult } from "express-validator";
 import { getRepository } from "typeorm";
 import { Academic } from "../entity/academic";
+import * as controllers from "../controllers/academic";
+
 
 const academicRouter = Router();
 
@@ -10,11 +12,8 @@ academicRouter.get("/", async (request, response) => {
 });
 
 academicRouter.post("/", async (request, response) => {
-  const repository = getRepository(Academic);
-  const academicToSave = repository.create(request.body);
-  const academic = await repository.save(academicToSave);
-
-  return response.status(201).json(academic);
+  const academic = await controllers.create(request.body);
+  return response.status(201).json(academic)
 });
 
 export default academicRouter;
