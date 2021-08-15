88ca77fc8b6b0e0900bb6eedcf454f116fbc80d4
devAndre-Isaac <andreisaac344@gmail.com>
2021-08-15 18:34:07 -0300

    feat: :sparkles: add error validation for academic

--- src/routes/academic.routes.ts
+++ src/routes/academic.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { validationResult } from "express-validator";
 import * as controllers from "../controllers/academic";
 
 const academicRouter = Router();
@@ -8,6 +9,10 @@ academicRouter.get("/", async (request, response) => {
 });
 
 academicRouter.post("/", async (request, response) => {
+  const errors = validationResult(request);
+  if (!errors.isEmpty()) {
+    return response.status(400).json({ errors: errors.array() });
+  }
   const academic = await controllers.create(request.body);
   return response.status(201).json(academic);
 });
